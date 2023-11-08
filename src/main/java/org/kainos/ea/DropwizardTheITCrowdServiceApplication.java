29c42fa8c34d63edd99ae3a598c598e412320183
Mia Green <miagreen14@googlemail.com>
2023-11-08 10:38:52 +0000

    [038] Syntax errors fixed to adhere by linting checks

--- src/main/java/org/kainos/ea/DropwizardTheITCrowdServiceApplication.java
+++ src/main/java/org/kainos/ea/DropwizardTheITCrowdServiceApplication.java
@@ -7,24 +7,23 @@ import org.kainos.ea.resources.ConnectionController;
 
 public class DropwizardTheITCrowdServiceApplication extends Application<DropwizardTheITCrowdServiceConfiguration> {
 
-    public static void main(final String[] args) throws Exception {
-        new DropwizardTheITCrowdServiceApplication().run(args);
-    }
+  public static void main(final String[] args) throws Exception {
+    new DropwizardTheITCrowdServiceApplication().run(args);
+  }
 
-    @Override
-    public String getName() {
-        return "DropwizardTheITCrowdService";
-    }
+  @Override
+  public String getName() {
+    return "DropwizardTheITCrowdService";
+  }
 
-    @Override
-    public void initialize(final Bootstrap<DropwizardTheITCrowdServiceConfiguration> bootstrap) {
-        // TODO: application initialization
-    }
+  @Override
+  public void initialize(final Bootstrap<DropwizardTheITCrowdServiceConfiguration> bootstrap) {
+  // TODO: application initialization
+  }
 
-    @Override
-    public void run(final DropwizardTheITCrowdServiceConfiguration configuration,
-                    final Environment environment) {
-        environment.jersey().register(new ConnectionController());
-    }
-
+  @Override
+  public void run(final DropwizardTheITCrowdServiceConfiguration configuration,
+                  final Environment environment) {
+    environment.jersey().register(new ConnectionController());
+  }
 }
--- src/main/java/org/kainos/ea/resources/ConnectionController.java
+++ src/main/java/org/kainos/ea/resources/ConnectionController.java
@@ -11,18 +11,15 @@ import javax.ws.rs.core.Response;
 
 @Path("/api")
 public class ConnectionController {
-
-    private final ConnectionService connectionService = new ConnectionService();
-
-    @GET
-    @Path("/database-active")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response testDatabaseConnection() {
-
-        try {
-            return Response.status(200).entity(connectionService.testDatabaseConnection()).build();
-        } catch (DatabaseConnectionFailedException e) {
-            throw new RuntimeException(e);
-        }
+  private final ConnectionService connectionService = new ConnectionService();
+  @GET
+  @Path("/database-active")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response testDatabaseConnection() {
+    try {
+      return Response.status(200).entity(connectionService.testDatabaseConnection()).build();
+    } catch (DatabaseConnectionFailedException e) {
+      throw new RuntimeException(e);
     }
+  }
 }
