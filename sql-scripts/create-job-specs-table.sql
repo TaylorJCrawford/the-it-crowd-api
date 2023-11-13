b3c19c66edf164a7deee17f5ee15d157d8d03b06
dcorry586 <dylancorr15@gmail.com>
2023-11-13 16:48:36 +0000

    added appropriate job spec classes to build job spec api for getting all jobs

---
+++ sql-scripts/create-job-specs-table.sql
@@ -0,0 +1,5 @@
+use TheITCrowd_MiaG;
+
+CREATE TABLE JobSpecs (jobSpecId INT AUTO_INCREMENT, jobSpecName VARCHAR(255) NOT NULL, PRIMARY KEY (jobSpecId));
+
+INSERT INTO JobSpecs (jobSpecName) VALUES ("Data Engineer");
--- src/main/java/org/kainos/ea/DropwizardTheITCrowdServiceApplication.java
+++ src/main/java/org/kainos/ea/DropwizardTheITCrowdServiceApplication.java
@@ -6,6 +6,7 @@ import io.dropwizard.setup.Environment;
 import io.federecio.dropwizard.swagger.SwaggerBundle;
 import io.federecio.dropwizard.swagger.SwaggerBundleConfiguration;
 import org.kainos.ea.resources.ConnectionController;
+import org.kainos.ea.resources.JobSpecController;
 
 public class DropwizardTheITCrowdServiceApplication extends Application<DropwizardTheITCrowdServiceConfiguration> {
 
@@ -32,5 +33,6 @@ public class DropwizardTheITCrowdServiceApplication extends Application<Dropwiza
   public void run(final DropwizardTheITCrowdServiceConfiguration configuration,
                   final Environment environment) {
     environment.jersey().register(new ConnectionController());
+    environment.jersey().register(new JobSpecController());
   }
 }
