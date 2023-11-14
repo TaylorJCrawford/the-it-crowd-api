f9b58e2f7e1c15d312a23ae2d73af553bc66ee1d
dcorry586 <dylancorr15@gmail.com>
2023-11-14 11:56:36 +0000

    resolved issues were first row was being skipped in get all job specs

--- src/main/java/org/kainos/ea/resources/JobSpecController.java
+++ src/main/java/org/kainos/ea/resources/JobSpecController.java
@@ -6,9 +6,11 @@ import org.kainos.ea.client.JobSpecsNotFoundException;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.sql.SQLException;
 
 @Api("Job Specification Controller")
 @Path("/api")
@@ -22,6 +24,20 @@ public class JobSpecController {
             return Response.ok(jobSpecService.getAllJobSpecs()).build();
         } catch (JobSpecsNotFoundException e) {
             System.out.println(e.getMessage());
+            return Response.serverError().build();
+        }
+    }
+    @GET
+    @Path("/job-specs/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getJobSpec(@PathParam("id") int id) {
+        try {
+            return Response.ok(jobSpecService.getJobSpec(id)).build();
+        } catch (JobSpecsNotFoundException e) {
+            System.out.println(e.getMessage());
+           return Response.status(Response.Status.BAD_REQUEST).build();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
             return Response.serverError().build();
         }
     }
