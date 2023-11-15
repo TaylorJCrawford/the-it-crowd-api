3d54a953c672e686afd499eccfea55c6a700dc02
dcorry586 <dylancorr15@gmail.com>
2023-11-15 12:09:09 +0000

    finished integration tests

--- src/test/java/org/kainos/ea/integration/JobSpecIntegrationTest.java
+++ src/test/java/org/kainos/ea/integration/JobSpecIntegrationTest.java
@@ -9,7 +9,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.kainos.ea.DropwizardTheITCrowdServiceApplication;
 import org.kainos.ea.DropwizardTheITCrowdServiceConfiguration;
 import org.kainos.ea.cli.JobSpec;
+import org.kainos.ea.db.JobSpecDao;
 
+import javax.ws.rs.core.Response;
 import java.util.List;
 
 @ExtendWith(DropwizardExtensionsSupport.class)
@@ -29,5 +31,24 @@ public class JobSpecIntegrationTest {
                 .get(List.class);
 
         Assertions.assertTrue(response.size() > 0);
+    }
+
+    @Test
+    void getJobSpec_shouldReturnJobSpec() {
+        String targetUrl = host + "/api/job-specs/8";
+        Response response = APP.client().target(targetUrl)
+                .request()
+                .get();
+        Assertions.assertEquals(200, response.getStatus());
+        Assertions.assertEquals(8, response.readEntity(JobSpec.class).getJobSpecID());
+    }
+    @Test
+    void getJobSpec_shouldReturnErrorWhenNotFound() {
+        String targetUrl = host + "/api/job-specs/88";
+        Response response = APP.client().target(targetUrl)
+                .request()
+                .get();
+
+        Assertions.assertEquals(400, response.getStatus());
     }
 }
