0faab64135322f56adcd8226ebcbb01ff272bb64
dcorry586 <dylancorr15@gmail.com>
2023-11-16 16:15:32 +0000

    removed dependency reduced pom file

--- src/test/java/org/kainos/ea/integration/JobIntegrationTest.java
+++ src/test/java/org/kainos/ea/integration/JobIntegrationTest.java
@@ -1,6 +1,7 @@
 package org.kainos.ea.integration;
 
 import java.util.List;
+
 import io.dropwizard.configuration.ResourceConfigurationSourceProvider;
 import io.dropwizard.testing.junit5.DropwizardAppExtension;
 import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
@@ -9,7 +10,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.kainos.ea.DropwizardTheITCrowdServiceApplication;
 import org.kainos.ea.DropwizardTheITCrowdServiceConfiguration;
+import org.kainos.ea.api.JobRequest;
 import org.kainos.ea.cli.Job;
+
+import javax.ws.rs.core.Response;
 
 @ExtendWith(DropwizardExtensionsSupport.class)
 public class JobIntegrationTest {
@@ -22,7 +26,7 @@ public class JobIntegrationTest {
     );
 
     @Test
-    void getJobRoles_shouldReturnListOfJobRoles()  {
+    void getJobRoles_shouldReturnListOfJobRoles() {
 
         // Construct the target URL with the host URL
         String targetUrl = host + "/api/jobs";
@@ -38,5 +42,30 @@ public class JobIntegrationTest {
         Assertions.assertEquals(expectedSize, response.size());
     }
 
+    @Test
+    void getJobRoleById_shouldReturnJobRequest() {
+        int id = 1;
+        String targetUrl = host + "/api/jobs/" + id;
 
+        JobRequest response = APP.client().target(targetUrl)
+                .request()
+                .get(JobRequest.class);
+
+        // You can change the expected value according to your test data
+        int expectedId = 1;
+        Assertions.assertEquals(expectedId, response.getJobId());
+    }
+
+    @Test
+    void getJobRoleById_shouldReturnErrorWhenNotFound() {
+        int id = 111;
+        String targetUrl = host + "/api/jobs/" + id;
+
+        Response response = APP.client().target(targetUrl)
+                .request()
+                .get(Response.class);
+
+        // You can change the expected value according to your test data
+        Assertions.assertEquals(400, response.getStatus());
+    }
 }
