19fcb8576db6e5bb2d5ec81d7b8715a5bcb56dc4
dcorry586 <dylancorr15@gmail.com>
2023-11-20 16:41:49 +0000

    pushing changes

--- src/main/java/org/kainos/ea/cli/Job.java
+++ src/main/java/org/kainos/ea/cli/Job.java
@@ -1,12 +1,19 @@
 package org.kainos.ea.cli;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Job {
   private int jobId;
   private String jobName;
 
   private String jobSpecUrl;
 
-  public Job(int jobId, String jobName, String jobSpecUrl) {
+  @JsonCreator
+  public Job(
+          @JsonProperty("jobId") int jobId,
+          @JsonProperty("jobName") String jobName,
+          @JsonProperty("jobSpecUrl") String jobSpecUrl) {
     this.jobId = jobId;
     this.jobName = jobName;
     this.jobSpecUrl = jobSpecUrl;
