f1619d34a951b4a46d40b6b2d060df36fbfe6c5c
dcorry586 <dylancorr15@gmail.com>
2023-11-21 10:12:11 +0000

    formatted job controller

--- src/main/java/org/kainos/ea/controller/JobController.java
+++ src/main/java/org/kainos/ea/controller/JobController.java
@@ -1,9 +1,11 @@
 package org.kainos.ea.controller;
+
 import io.swagger.annotations.Api;
 import org.kainos.ea.api.JobService;
 import org.kainos.ea.client.CantGetAnyRolesException;
 import org.kainos.ea.db.DatabaseConnector;
 import org.kainos.ea.db.JobDao;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -18,7 +20,7 @@ public class JobController {
 
   private final JobService jobService;
 
-  public JobController(){
+  public JobController() {
     DatabaseConnector databaseConnector = new DatabaseConnector();
     jobService = new JobService(new JobDao(), databaseConnector);
   }
@@ -27,15 +29,13 @@ public class JobController {
   @GET
   @Path("/jobs")
   @Produces(MediaType.APPLICATION_JSON)
-  public Response getJobs(){
-    try{
+  public Response getJobs() {
+    try {
       return Response.ok(jobService.getAllJobs()).build();
-    }
-    catch(SQLException e){
+    } catch (SQLException e) {
       System.err.println(e.getMessage());
       return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
-    }
-    catch(CantGetAnyRolesException e){
+    } catch (CantGetAnyRolesException e) {
       System.err.println(e.getMessage());
       return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
               .entity("Job roles retrieval error: " + e.getMessage())
@@ -46,15 +46,13 @@ public class JobController {
   @GET
   @Path("/jobs/{id}")
   @Produces(MediaType.APPLICATION_JSON)
-  public Response getJobById(@PathParam("id") int id){
-    try{
+  public Response getJobById(@PathParam("id") int id) {
+    try {
       return Response.ok(jobService.getJobById(id)).build();
-    }
-    catch(SQLException e){
+    } catch (SQLException e) {
       System.err.println(e.getMessage());
       return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
-    }
-    catch(CantGetAnyRolesException e){
+    } catch (CantGetAnyRolesException e) {
       System.err.println(e.getMessage());
       return Response.status(Response.Status.BAD_REQUEST).build();
     }
