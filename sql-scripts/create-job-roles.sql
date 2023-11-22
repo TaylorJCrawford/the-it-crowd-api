4da78ce9cacf992cdad9c2c455adf504477851a0
dcorry586 <dylancorr15@gmail.com>
2023-11-22 16:12:30 +0000

    updated solution architectname

--- sql-scripts/create-job-roles.sql
+++ sql-scripts/create-job-roles.sql
@@ -12,5 +12,5 @@ INSERT INTO JobRoles (jobName, jobSpecUrl) VALUES
     ('Data Architect', "https://kainossoftwareltd.sharepoint.com/:b:/r/people/Job%20Specifications/Data%20and%20Artificial%20Intelligence/Job%20profile%20-%20Data%20Architect%20(M).pdf?csf=1&web=1&e=8kGbsx"),
     ('Platform Engineer', "https://kainossoftwareltd.sharepoint.com/:b:/r/people/Job%20Specifications/Platforms/Job%20profile%20-%20Platform%20Engineer%20(Associate).pdf?csf=1&web=1&e=Vt5AUQ"),
     ('UX Designer', "https://kainossoftwareltd.sharepoint.com/:b:/r/people/Job%20Specifications/Engineering/Job%20profile%20%20Test%20Engineer%20(Associate).pdf?csf=1&web=1&e=P3Pe76");
-    ('Technical Architect', "https://kainossoftwareltd.sharepoint.com/:b:/r/people/Job%20Specifications/Platforms/Job%20profile%20-%20Platform%20Solution%20Architect%20(Manager).pdf?csf=1&web=1&e=Wr9lCA");
+    ('Platform Solution Architect', "https://kainossoftwareltd.sharepoint.com/:b:/r/people/Job%20Specifications/Platforms/Job%20profile%20-%20Platform%20Solution%20Architect%20(Manager).pdf?csf=1&web=1&e=Wr9lCA");
     ('Test Engineer', "https://kainossoftwareltd.sharepoint.com/:b:/r/people/Job%20Specifications/Engineering/Job%20profile%20%20Test%20Engineer%20(Associate).pdf?csf=1&web=1&e=P3Pe76");
